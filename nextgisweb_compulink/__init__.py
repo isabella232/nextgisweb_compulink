4246b04ee179bb0325e291511afc7772c643fafd
yellow-sky <nikulin.e@gmail.com>
2014-11-10 17:18:19 +0300

    Init struct of extension

---
+++ nextgisweb_compulink/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+def pkginfo():
+    #return dict(components=dict(file_bucket="nextgisweb_compulink.file_bucket"))
+    return dict(components=dict())
+
+def amd_packages():
+    return (
+        #('ngw-file-bucket', 'nextgisweb_compulink:file_bucket/amd/ngw-file-bucket'),
+    )
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+requires = (
+    'nextgisweb',
+    'nextgisweb_rekod'
+)
+
+entry_points = {
+    'nextgisweb.packages': [
+        'nextgisweb_compulink = nextgisweb_compulink:pkginfo',
+    ],
+
+    'nextgisweb.amd_packages': [
+        'nextgisweb_compulink = nextgisweb_compulink:amd_packages',
+    ],
+
+}
+
+setup(
+    name='nextgisweb_compulink',
+    version=version,
+    description="",
+    long_description="",
+    classifiers=[],
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    entry_points=entry_points,
+)
