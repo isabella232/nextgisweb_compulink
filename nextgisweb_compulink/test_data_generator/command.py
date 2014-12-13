f2571c0f2d7247c0c1cdffdae2e8b1e253130354
yellow-sky <nikulin.e@gmail.com>
2014-12-14 01:27:55 +0300

    Генератор создает внутреннюю структуру групповых типов

--- nextgisweb_compulink/test_data_generator/command.py
+++ nextgisweb_compulink/test_data_generator/command.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy.orm import sessionmaker
-import transaction
 
 from nextgisweb.command import Command
-from nextgisweb.resource import Resource, ResourceGroup
-from nextgisweb.models import declarative_base, DBSession
-from nextgisweb_compulink.compulink_admin.model import FoclProject, SituationPlan, FoclStruct
-
+from test_data_generator import TestDataGenerator
 from osm_data_source import OsmDataSource
 
 
@@ -16,83 +11,23 @@ class GenerateTestDataCommand():
 
     @classmethod
     def argparser_setup(cls, parser, env):
-        parser.add_argument('--conn-str', default='', help='')
-        parser.add_argument('--parent-res-id', default='0', help='')
-        parser.add_argument('--res-name', default='', help='')
-        parser.add_argument('--vols-count', type=int, default=10, help='')
+        parser.add_argument('--conn-str', default='')
+        parser.add_argument('--parent-res-id', default='0')
+        parser.add_argument('--res-name', default='')
+        parser.add_argument('--vols-count', type=int, default=10)
+        parser.add_argument('--country-osm-id', type=int)
 
     @classmethod
     def execute(cls, args, env):
         data_src = OsmDataSource(args.conn_str)
+        test_data_gen = TestDataGenerator(data_src,
+                                          args.parent_res_id,
+                                          args.res_name,
+                                          args.country_osm_id,
+                                          args.vols_count)
+        test_data_gen.generate_data()
 
 
-        #Session = sessionmaker(bind=DBSession.connection())
-        session = DBSession  #  Session()
-
-        root_res = session.query(Resource).get(args.parent_res_id)
-
-        group_res = ResourceGroup(parent=root_res, owner_user=root_res.owner_user, display_name=args.res_name)
-        #group_res.
-        group_res.persist()
-
-
-        regions = data_src.GetRegions(22468)  # HARDCODE!!!Russia code in db
-        created_structs = 0
-        appended_regs = []
-        for reg in regions:
-            if reg.name in appended_regs or not reg.name:
-                continue
-            else:
-                appended_regs.append(reg.name)
-
-            print reg.ogc_fid, ' ', reg.name
-
-            reg_group_res = ResourceGroup(parent=group_res, owner_user=group_res.owner_user, display_name=reg.name)
-            reg_group_res.persist()
-
-            districts = data_src.GetDistricts(reg.ogc_fid)
-            appended_dist = []
-            for dist in districts:
-                print '    ', dist.ogc_fid, ' ', dist.name
-
-                if dist.name in appended_dist or not dist.name:
-                    continue
-                else:
-                    appended_dist.append(dist.name)
-
-                vols_proj = FoclProject(parent=reg_group_res, owner_user=reg_group_res.owner_user, display_name=dist.name)
-                vols_proj.persist()
-
-                sit_plan = SituationPlan(parent=vols_proj, owner_user=vols_proj.owner_user, display_name='Ситуационный план')
-                sit_plan.persist()
-
-                points = data_src.GetPoints(dist.ogc_fid)
-                appended_vols = []
-                for point in points:
-                    if point.name in appended_vols or not point.name:
-                        continue
-                    else:
-                        appended_vols.append(point.name)
-
-                    vols_strut = FoclStruct(parent=vols_proj, owner_user=vols_proj.owner_user, display_name=u'ВОЛС ' + unicode(point.name))
-                    vols_strut.persist()
-
-                    #check
-                    created_structs += 1
-                    if created_structs >= args.vols_count:
-                        break
-                #roads = data_src.GetRoads(dist.ogc_fid)
-
-
-                #check
-                created_structs += 1
-                if created_structs >= args.vols_count:
-                    break
-
-            if created_structs >= args.vols_count:
-                break
-
-        transaction.commit()
 
 
 
--- nextgisweb_compulink/test_data_generator/test_data_generator.py
+++ nextgisweb_compulink/test_data_generator/test_data_generator.py
@@ -1,12 +1,116 @@
+# coding=utf-8
+import transaction
+from nextgisweb import DBSession
+from nextgisweb.resource import Resource
+from nextgisweb.resource import ResourceGroup
+from nextgisweb_compulink.compulink_admin.model import FoclProject, FoclStruct, FoclProjectSerializer, \
+    SituationPlanSerializer, FoclStructSerializer
+from nextgisweb_compulink.compulink_admin.model import SituationPlan
+
 __author__ = 'yellow'
 __license__ = ''
 __date__ = '2014'
 
-
-
 class TestDataGenerator():
 
-    def __init__(self, data_source, parent_resource_id, root_res_name):
+    def __init__(self, data_source, parent_resource_id, root_res_name, country_id, focl_struct_count):
+        self.data_src = data_source
+        self.parent_res_id = parent_resource_id
+        self.res_name = root_res_name
+        self.counry_id = country_id
+        self.focl_srtuct_count = focl_struct_count
         pass
 
+    def generate_data(self):
+        self.created_structs = 0
 
+        session = DBSession
+
+        #parent element
+        root_res = session.query(Resource).get(self.parent_res_id)
+
+        #group of resources
+        group_res = ResourceGroup(parent=root_res,
+                                  owner_user=root_res.owner_user,
+                                  display_name=self.res_name)
+        group_res.persist()
+
+        self.create_regions(self.counry_id, group_res)
+
+        transaction.commit()
+
+
+    def create_regions(self, country_id, parent_group):
+        regions = self.data_src.GetRegions(country_id) #22468)  # HARDCODE!!!Russia code in db
+
+        appended_regs = []
+        for reg in regions:
+            if reg.name in appended_regs or not reg.name:
+                continue
+            else:
+                appended_regs.append(reg.name)
+                print reg.name
+
+            reg_group_res = ResourceGroup(parent=parent_group,
+                                          owner_user=parent_group.owner_user,
+                                          display_name=reg.name)
+            reg_group_res.persist()
+            self.create_districts(reg.ogc_fid, reg_group_res)
+
+            #check count
+            if self.created_structs >= self.focl_srtuct_count:
+                break
+
+    def create_districts(self, region_id, reg_group_res):
+        districts = self.data_src.GetDistricts(region_id)
+        appended_dist = []
+        for dist in districts:
+            if dist.name in appended_dist or not dist.name:
+                continue
+            else:
+                appended_dist.append(dist.name)
+                print '    ', dist.name
+
+            #create focl_proj
+            vols_proj = FoclProject(parent=reg_group_res,
+                                    owner_user=reg_group_res.owner_user,
+                                    display_name=dist.name)
+            vols_proj.persist()
+            FoclProjectSerializer.create_focl_project_content(vols_proj)
+
+            #create sit_plan
+            sit_plan = SituationPlan(parent=vols_proj,
+                                     owner_user=vols_proj.owner_user,
+                                     display_name=u'Ситуационный план')
+            sit_plan.persist()
+            SituationPlanSerializer.create_situation_plan_content(sit_plan)
+
+            self.create_focl_structs(dist.ogc_fid, vols_proj)
+
+            #check count
+            if self.created_structs >= self.focl_srtuct_count:
+                break
+
+
+    def create_focl_structs(self, district_id, vols_proj):
+
+        points = self.data_src.GetPoints(district_id)
+        appended_vols = []
+        for point in points:
+            if point.name in appended_vols or not point.name:
+                continue
+            else:
+                appended_vols.append(point.name)
+
+            vols_struct = FoclStruct(parent=vols_proj,
+                                    owner_user=vols_proj.owner_user,
+                                    display_name=u'ВОЛС ' + unicode(point.name))
+            vols_struct.persist()
+            FoclStructSerializer.create_focl_struct_content(vols_struct)
+
+            #roads = self.data_src.GetRoads(dist.ogc_fid)
+
+            #check count
+            self.created_structs += 1
+            if self.created_structs >= self.focl_srtuct_count:
+                break
