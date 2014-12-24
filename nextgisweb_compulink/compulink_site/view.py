e3b92c1469076a23c9c84adb2d82221a90140a4e
yellow-sky <nikulin.e@gmail.com>
2014-12-24 19:00:47 +0300

    Compulink Site init struct

---
+++ nextgisweb_compulink/compulink_site/view.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from os import path
+from pyramid.renderers import render_to_response
+
+CURR_PATH = path.dirname(__file__)
+
+
+def setup_pyramid(comp, config):
+    config.add_route(
+        'compulink.site.map',
+        '/compulink/monitoring_map').add_view(show_map)
+
+    config.add_static_view(
+        '/compulink/static',
+        path.join(CURR_PATH, 'static'), cache_max_age=3600)
+
+
+def show_map(request):
+    values = {}
+    return render_to_response('templates/monitoring_webmap/display.mako', values, request=request)
