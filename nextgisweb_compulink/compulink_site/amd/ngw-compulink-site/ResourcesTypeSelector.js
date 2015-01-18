6e7664b81792ee5cef06568a86d4ae42b380d9d2
karavanjo <jownkiv@gmail.com>
2015-01-19 01:42:21 +0300

    #3 resource types dropdown logic was encapsulated to ResourcesTypeSelector module

---
+++ nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/ResourcesTypeSelector.js
@@ -0,0 +1,28 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/topic',
+    'dojo/Deferred',
+    'dojo/request/xhr',
+    'dojo/on',
+    'dijit/registry',
+    'dojo/topic',
+    'ngw-compulink-libs/jstree-3.0.9/jstree'
+], function (declare, lang, topic, Deferred, xhr, on, registry, topic,
+             jstree) {
+
+    return declare('ResourcesTypeSelector', [], {
+        _selectDijit: null,
+
+        constructor: function (domId) {
+            this._selectDijit = registry.byId(domId);
+            this.bindEvents();
+        },
+
+        bindEvents: function () {
+            on(this._selectDijit, 'change', lang.hitch(this, function (selectedType) {
+                topic.publish('resources/type/set', selectedType);
+            }));
+        }
+    });
+});
