39baa0bd88bbc250b891caf9e319baefc27406fb
karavanjo <jownkiv@gmail.com>
2015-01-23 16:04:16 +0300

    #1 JsTreeValidationConfirmDialog was implemented

---
+++ nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/JsTreeValidationConfirmDialog.js
@@ -0,0 +1,24 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/array',
+    'dojo/_base/lang',
+    'dojo/topic',
+    'ngw-compulink-site/ConfirmDialog'
+], function (declare, array, lang, topic, ConfirmDialog) {
+    if (!_instance) {
+        var _instance = new ConfirmDialog({
+            title: 'Внимание!',
+            message: 'Выбрано большое количество слоев!<br/>загрузка дополнительных может замедлить работу приложения.<br/>Можете продолжить или выключить ненужные.<br/><br/>Загрузить новые слои?',
+            buttonOk: 'Да',
+            buttonCancel: 'Нет',
+            handlerOk: function () {
+                topic.publish('resources/tree/validation/checklimit/true');
+
+            },
+            handlerCancel: function () {
+                topic.publish('resources/tree/validation/checklimit/false');
+            }
+        });
+    }
+    return _instance;
+});
