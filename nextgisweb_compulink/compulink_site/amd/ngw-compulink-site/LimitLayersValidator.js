3ce489cc68afab5f5ea2b9f1e74b42f0bee9617e
karavanjo <jownkiv@gmail.com>
2015-01-26 01:03:20 +0300

    #1 methods prototype for validation of controls were added

--- nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/LimitLayersValidator.js
+++ nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/LimitLayersValidator.js
@@ -9,8 +9,32 @@ define([
 ], function (declare, lang, topic, Deferred, xhr, on, registry) {
     return declare([], {
         settings: {},
+        validatorName: 'LimitLayersValidator',
 
         constructor: function (ResourcesTree, LayersSelector) {
+            this.ResourcesTree = ResourcesTree;
+            this.LayersSelector = LayersSelector;
+        },
+
+        validate: function (initiator, data) {
+            switch (initiator) {
+                case 'ResourcesTree':
+                    return this.validateResourcesTree(data);
+                    break;
+                case 'LayersSelector':
+                    return this.validateLayersSelector(data);
+                    break;
+                default:
+                    console.log('LimitLayersValidator is not recognized "' + initiator + '" initiator. Returned "true".');
+                    break;
+            }
+        },
+
+        validateResourcesTree: function (data) {
+
+        },
+
+        validateLayersSelector: function (data) {
 
         }
     });
