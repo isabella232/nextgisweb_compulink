e770304fe4f7e69c8f904b3d6c556a18d85c5e27
karavanjo <jownkiv@gmail.com>
2015-01-26 05:01:47 +0300

    #1 validating and showing ConfirmDialog was moved from ResourcesTree

--- nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/LimitLayersValidator.js
+++ nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/LimitLayersValidator.js
@@ -1,12 +1,14 @@
 define([
     'dojo/_base/declare',
     'dojo/_base/lang',
+    'dojo/_base/array',
     'dojo/topic',
     'dojo/Deferred',
     'dojo/request/xhr',
     'dojo/on',
-    'dijit/registry'
-], function (declare, lang, topic, Deferred, xhr, on, registry) {
+    'dijit/registry',
+    'ngw-compulink-site/JsTreeValidationConfirmDialog'
+], function (declare, lang, array, topic, Deferred, xhr, on, registry, JsTreeValidationConfirmDialog) {
     return declare([], {
         settings: {},
         validatorName: 'LimitLayersValidator',
@@ -36,6 +38,38 @@ define([
 
         validateLayersSelector: function (data) {
 
+        },
+
+        bindEvents: function (initiator) {
+            switch (initiator) {
+                case 'ResourcesTree':
+                    return this.bindEventsResourcesTree();
+                    break;
+                case 'LayersSelector':
+                    return this.bindEventsLayersSelector();
+                    break;
+                default:
+                    console.log('LimitLayersValidator is not recognized "' + initiator + '" initiator. Returned "true".');
+                    break;
+            }
+        },
+
+        bindEventsResourcesTree: function () {
+
+        },
+
+        validateSelectedBottomNodeLimit: function () {
+            var checkedNodesCount = this.$tree.jstree(true).get_bottom_selected().length;
+            if (this.settings.limitCheckedNodesCount && this.settings.limitCheckedNodesCount < checkedNodesCount) {
+                this.showConfirmDialog();
+                return false;
+            } else {
+                return true;
+            }
+        },
+
+        showConfirmDialog: function () {
+            JsTreeValidationConfirmDialog.show();
         }
     });
 });
