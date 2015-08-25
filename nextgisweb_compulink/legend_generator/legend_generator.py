803163f545964661c578f41e7a3d2020423b4e43
yellow-sky <nikulin.e@gmail.com>
2015-08-25 20:18:31 +0300

    Fix legend (fonst\styles and req's)

--- nextgisweb_compulink/legend_generator/legend_generator.py
+++ nextgisweb_compulink/legend_generator/legend_generator.py
@@ -37,52 +37,54 @@ class LegendGenerator:
         caption_font = ImageFont.truetype('LiberationSans-Regular.ttf', 12)
         label_font = ImageFont.truetype('LiberationSans-Regular.ttf', 10)
 
-        label_plan = u'проектные'
-        label_fact = u'построенные'
+        label_plan_m = u'проектируемый'
+        label_plan_w = u'проектируемая'
+        label_fact_m = u'построенный'
+        label_fact_w = u'построенная'
 
 
         #Start draw
         current_h = 10
-        caption = u'Точки доступа'
+        caption = u'Точка доступа'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((50, current_h+20), label_plan, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((40, current_h+20), label_plan_w, font=label_font, fill=(0, 0, 0, 255))
         plan_img = self.legend_img_from_style('access_point', 0, 20, 20)
         background.paste(plan_img, (18, current_h+15))
 
-        drawer.text((155, current_h+20), label_fact, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((155, current_h+20), label_fact_w, font=label_font, fill=(0, 0, 0, 255))
         fact_img = self.legend_img_from_style('real_access_point', 0, 20, 20)
-        background.paste(fact_img, (123, current_h+15))
+        background.paste(fact_img, (133, current_h+15))
 
         current_h += 50
-        caption = u'Оптические кроссы'
+        caption = u'Оптический кросс'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((50, current_h+20), label_plan, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((40, current_h+20), label_plan_m, font=label_font, fill=(0, 0, 0, 255))
         plan_img = self.legend_img_from_style('optical_cross', 0, 16, 16)
         background.paste(plan_img, (20, current_h+18))
 
-        drawer.text((155, current_h+20), label_fact, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((155, current_h+20), label_fact_m, font=label_font, fill=(0, 0, 0, 255))
         fact_img = self.legend_img_from_style('real_optical_cross', 0, 16, 16)
-        background.paste(fact_img, (125, current_h+18))
+        background.paste(fact_img, (135, current_h+18))
 
 
         current_h += 50
-        caption = u'Оптические муфты'
+        caption = u'Оптическая муфта'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((50, current_h+20), label_plan, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((40, current_h+20), label_plan_w, font=label_font, fill=(0, 0, 0, 255))
         plan_img = self.legend_img_from_style('fosc', 0, 16, 16)
         background.paste(plan_img, (20, current_h+18))
 
-        drawer.text((155, current_h+20), label_fact, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((155, current_h+20), label_fact_w, font=label_font, fill=(0, 0, 0, 255))
         fact_img = self.legend_img_from_style('real_fosc', 0, 16, 16)
-        background.paste(fact_img, (125, current_h+18))
+        background.paste(fact_img, (135, current_h+18))
 
         current_h += 50
         caption = u'Трасса оптического кабеля'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((100, current_h+15), label_plan, font=label_font, fill=(0, 0, 0, 255))
-        drawer.text((170, current_h+15), label_fact, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((90, current_h+15), label_plan_w, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((170, current_h+15), label_fact_w, font=label_font, fill=(0, 0, 0, 255))
 
-        classes = {0: u'В грунте', 1:u'На опоре ЛЭП', 2:u'В каб. канализации', 3:u'В здании', 4:u'Прочее'}
+        classes = {0: u'В грунте', 1:u'На опорах ЛЭП', 2:u'В каб. канализации', 3:u'В здании', 4:u'Прочее'}
         for lay_ind, lay_name in classes.items():
             drawer.text((10, current_h + 15 + (lay_ind + 1)*11), lay_name, font=label_font, fill=(0, 0, 0, 255))
 
@@ -93,38 +95,38 @@ class LegendGenerator:
             background.paste(fact_img, (185, current_h + 15 + (lay_ind + 1)*11))
 
         current_h += 100
-        caption = u'Спецпереходы'
+        caption = u'Спецпереход'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((50, current_h+20), label_plan, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((47, current_h+20), label_plan_m, font=label_font, fill=(0, 0, 0, 255))
         plan_img = self.legend_img_from_style('special_transition', 0, 40, 10)
         background.paste(plan_img, (5, current_h+20))
 
-        drawer.text((155, current_h+20), label_fact, font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((170, current_h+20), label_fact_m, font=label_font, fill=(0, 0, 0, 255))
         fact_img = self.legend_img_from_style('real_special_transition', 0, 40, 10)
-        background.paste(fact_img, (110, current_h+20))
+        background.paste(fact_img, (127, current_h+20))
         background.save(out_path)
 
         current_h += 50
         caption = u'Объекты размещения'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((40, current_h+20), u'Колодцы Каб. кан.', font=label_font, fill=(0, 0, 0, 255))
-        plan_img = self.legend_img_from_style('transmission_tower', 0, 16, 16) #sump_canalization
+        drawer.text((40, current_h+20), u'Колодец Каб. кан.', font=label_font, fill=(0, 0, 0, 255))
+        plan_img = self.legend_img_from_style('sump_canalization', 0, 16, 16)
         background.paste(plan_img, (20, current_h+18))
 
-        drawer.text((160, current_h+20), u'Опоры ВЛ', font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((160, current_h+20), u'Опора ВЛ', font=label_font, fill=(0, 0, 0, 255))
         fact_img = self.legend_img_from_style('transmission_tower', 0, 16, 16)
         background.paste(fact_img, (140, current_h+18))
         background.save(out_path)
 
 
         current_h += 50
-        caption = u'Точки А-Б'
+        caption = u'Начальная и конечная точки трассы'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((60, current_h+35), u'Точка А', font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((60, current_h+35), u'Начальная\n точка', font=label_font, fill=(0, 0, 0, 255))
         plan_img = self.legend_img_from_style('endpoint', 0, 36, 96)
         background.paste(plan_img, (20, current_h+18))
 
-        drawer.text((180, current_h+35), u'Точка Б', font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((180, current_h+35), u'Конечная\n точка', font=label_font, fill=(0, 0, 0, 255))
         fact_img = self.legend_img_from_style('endpoint', 1, 36, 96)
         background.paste(fact_img, (140, current_h+18))
         background.save(out_path)
@@ -133,11 +135,11 @@ class LegendGenerator:
         current_h += 80
         caption = u'Отметки'
         self.draw_center_text(drawer, caption, caption_font, max_x, current_h)
-        drawer.text((40, current_h+20), u'Трассы ОК', font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((40, current_h+20), u'Точка трассы ОК', font=label_font, fill=(0, 0, 0, 255))
         plan_img = self.legend_img_from_style('real_optical_cable_point', 0, 16, 16)
         background.paste(plan_img, (20, current_h+18))
 
-        drawer.text((160, current_h+20), u'Спецпереходов', font=label_font, fill=(0, 0, 0, 255))
+        drawer.text((160, current_h+20), u'Точка входа\n (выхода)\n спецперехода', font=label_font, fill=(0, 0, 0, 255))
         fact_img = self.legend_img_from_style('real_special_transition_point', 0, 16, 16)
         background.paste(fact_img, (140, current_h+18))
         background.save(out_path)
@@ -158,6 +160,7 @@ class LegendGenerator:
         esymbolset = E.symbolset(resource_filename('nextgisweb_mapserver', 'symbolset'))
         emap.insert(0, esymbolset)
 
+
         # PIXMAP и SVG маркеры
         for type_elem in emap.iterfind('./symbol/type'):
             if type_elem.text not in ('pixmap', 'svg'):
@@ -167,9 +170,13 @@ class LegendGenerator:
             marker = Marker.filter_by(keyname=image.text).one()
             image.text = self.env.file_storage.filename(marker.fileobj)
 
+        # FONTS
+        fonts_e = E.fontset(self.env.mapserver.settings['fontset'])
+        emap.insert(0, fonts_e)
+
         mapf_map = Map().from_xml(emap)
         mapfile(mapf_map, buf)
-        mapobj = mapscript.fromstring(buf.getvalue())
+        mapobj = mapscript.fromstring(buf.getvalue().encode('utf-8'))
         layerobj = mapobj.getLayer(0)
         classobj = layerobj.getClass(subclass)
         gdimg = classobj.createLegendIcon(mapobj, layerobj, w, h)
@@ -178,16 +185,3 @@ class LegendGenerator:
         img = Image.open(mem_png)
 
         return img
-
-
-
-
-# offset
-# from PIL import Image
-# img = Image.open('/pathto/file', 'r')
-# img_w, img_h = img.size
-# background = Image.new('RGBA', (1440, 900), (255, 255, 255, 255))
-# bg_w, bg_h = background.size
-# offset = ((bg_w - img_w) / 2, (bg_h - img_h) / 2)
-# background.paste(img, offset)
-# background.save('out.png')
