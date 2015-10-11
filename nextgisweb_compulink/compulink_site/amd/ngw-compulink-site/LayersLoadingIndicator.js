7a3468f9ca20bc5cbbff04cf1f61444fd7548500
Ivan Kavaliou <jownkiv@gmail.com>
2015-10-11 18:49:59 +0300

    #171 logic for progress bar was implemented

--- nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/LayersLoadingIndicator.js
+++ nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/LayersLoadingIndicator.js
@@ -5,6 +5,8 @@ define([
     'dojo/_base/lang',
     'dojo/html',
     'dojo/dom-construct',
+    'dojo/dom-style',
+    'dojo/dom-attr',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
@@ -13,46 +15,105 @@ define([
     'ngw-compulink-libs/mustache/mustache',
     'dojo/text!./templates/LayersLoadingIndicator.mustache',
     'xstyle/css!./templates/css/LayersLoadingIndicator.css'
-], function (declare, query, array, lang, html, domConstruct, _Widget, _TemplatedMixin, _WidgetsInTemplateMixin,
-             Dialog, on, mustache, template) {
+], function (declare, query, array, lang, html, domConstruct, domStyle, domAttr, _Widget,
+             _TemplatedMixin, _WidgetsInTemplateMixin, Dialog, on, mustache, template) {
     return declare([], {
         openLayersMap: null,
         domElement: null,
         domLoaded: null,
-        countLoaded: 0,
+        loading: {
+            count: 0,
+            layers: {}
+        },
 
         constructor: function (map) {
             this.openLayersMap = map.olMap;
             this._buildElementIndicator();
+            this._bindEvents();
         },
 
         _buildElementIndicator: function () {
             var viewPortDiv = this.openLayersMap.viewPortDiv;
             this.domElement = domConstruct.place(mustache.render(template), viewPortDiv);
-            this.domLoaded = query('span.loaded', this.domElement)[0];
+            this.domLoaded = query('span', this.domElement)[0];
         },
 
         _bindEvents: function () {
-            this.openLayersMap.events.register('addlayer', this.openLayersMap, lang.hitch(this, function (layer) {
+            this.openLayersMap.events.register('addlayer', this.openLayersMap, lang.hitch(this, function (e) {
+                var layer = e.layer;
                 this._layerBindEvents(layer);
+                this._increaseLayersCount();
+                this.loading.layers[layer.id] = true;
+                this._setPercent();
             }));
 
-            this.openLayersMap.events.register('removelayer', this.openLayersMap, lang.hitch(this, function (layer) {
+            this.openLayersMap.events.register('removelayer', this.openLayersMap, lang.hitch(this, function (e) {
+                var layer = e.layer;
+                if (this.loading.layers.hasOwnProperty(layer.id)) {
+                    delete this.loading.layers[layer.id];
+                    this._decreaseLayersCount();
+                    this._setPercent();
+                }
             }));
         },
 
         _layerBindEvents: function (layer) {
-            this.openLayersMap.events.register('loadstart', layer, lang.hitch(this, function () {
+            layer.events.register('loadstart', '', lang.hitch(this, function (e) {
+                var layer = e.object;
+                this._increaseLayersCount();
+                this.loading.layers[layer.id] = true;
+                this._setPercent();
+            }));
 
-            }))
+            layer.events.register('loadend', '', lang.hitch(this, function (e) {
+                var layer = e.object;
+                if (this.loading.layers.hasOwnProperty(layer.id)) {
+                    delete this.loading.layers[layer.id];
+                    this._decreaseLayersCount();
+                    this._setPercent();
+                }
+            }));
         },
 
         _getCountLayers: function () {
-            return this.openLayersMap.getNumLayers();
+            var layers = this.openLayersMap.layers,
+                countLayers = layers.length,
+                countDataLayers = 0;
+
+            for (var i = 0; i < countLayers; i++) {
+                if (!layers[i].isBaseLayer) {
+                    countDataLayers++;
+                }
+            }
+
+            return countDataLayers;
+        },
+
+        _decreaseLayersCount: function () {
+            this.loading.count--;
+            if (this.loading.count < 0) {
+                this.loading.count = 0;
+            }
+        },
+
+        _increaseLayersCount: function () {
+            this.loading.count++;
         },
 
         _setPercent: function () {
-            html.set(this.domLoaded, Math.round(this.countLoaded / this._getCountLayers() * 100).toString());
+            var percent,
+                loadingCount = this.loading.count,
+                countAllLayers = this._getCountLayers();
+
+            if (loadingCount == 0) {
+                percent = 100;
+            } else {
+                percent = Math.round((countAllLayers - loadingCount) / countAllLayers * 100);
+            }
+
+            percent = percent.toString();
+            domStyle.set(this.domLoaded, 'width', percent + '%');
+            domAttr.set(this.domElement, 'title', 'Загружено ' + percent + '% данных')
         }
     });
 });
