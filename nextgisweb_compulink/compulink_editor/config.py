d822480c069004c4f8c39b8a88082a6a7d5b8cf6
Ivan Kavaliou <jownkiv@gmail.com>
2015-11-16 11:20:25 +0300

    #223 added editable layers config

---
+++ nextgisweb_compulink/compulink_editor/config.py
@@ -0,0 +1,9 @@
+EDITABLE_LAYERS = [
+    'real_special_transition',
+    'real_special_transition_point',
+    'real_optical_cable',
+    'real_optical_cable_point',
+    'real_fosc',
+    'real_optical_cross',
+    'real_access_point'
+]
