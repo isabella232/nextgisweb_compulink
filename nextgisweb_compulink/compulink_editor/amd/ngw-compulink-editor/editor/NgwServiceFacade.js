61760131ca64358bf243a6daca0ca84442011d4b
Ivan Kavaliou <jownkiv@gmail.com>
2015-12-09 10:37:08 +0300

    closed #231 facade was extended by getFeature and getResourceInfo functions

--- nextgisweb_compulink/compulink_editor/amd/ngw-compulink-editor/editor/NgwServiceFacade.js
+++ nextgisweb_compulink/compulink_editor/amd/ngw-compulink-editor/editor/NgwServiceFacade.js
@@ -2,9 +2,8 @@ define([
     'dojo/_base/declare',
     'dojo/_base/lang',
     'dojo/request/xhr',
-    'dojox/dtl/_base',
-    'dojox/dtl/Context'
-], function (declare, lang, xhr, dtlBase, dtlContext) {
+    'dojox/dtl/_base'
+], function (declare, lang, xhr, dtlBase) {
 
     return declare([], {
         constructor: function (ngwApplicationUrl) {
@@ -13,12 +12,29 @@ define([
 
         GET_FEATURE: new dtlBase.Template('/api/resource/{{resourceId}}/feature/{{featureId}}', true),
         GET_ALL_FEATURES: new dtlBase.Template('/api/resource/{{resourceId}}/feature/', true),
+        GET_RESOURCE: new dtlBase.Template('/api/resource/{{resourceId}}', true),
 
         ngwApplicationUrl: null,
 
         getAllFeatures: function (resourceId) {
             var dtlContext = new dtlBase.Context({resourceId: resourceId}),
                 url = this.ngwApplicationUrl + this.GET_ALL_FEATURES.render(dtlContext);
+            return xhr.get(url, {
+                handleAs: 'json'
+            });
+        },
+
+        getFeature: function (resourceId, featureId) {
+            var dtlContext = new dtlBase.Context({resourceId: resourceId, featureId: featureId}),
+                url = this.ngwApplicationUrl + this.GET_FEATURE.render(dtlContext);
+            return xhr.get(url, {
+                handleAs: 'json'
+            });
+        },
+
+        getResourceInfo: function (resourceId) {
+            var dtlContext = new dtlBase.Context({resourceId: resourceId}),
+                url = this.ngwApplicationUrl + this.GET_RESOURCE.render(dtlContext);
             return xhr.get(url, {
                 handleAs: 'json'
             });
