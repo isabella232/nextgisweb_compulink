2d9cc339332bea5feb2d922e6abe0735ffd453eb
Ivan Kavaliou <jownkiv@gmail.com>
2015-12-30 23:14:11 +0300

    #274 added view and viewmodels for region ref book page

--- nextgisweb_compulink/compulink_admin/view_reference_books.py
+++ nextgisweb_compulink/compulink_admin/view_reference_books.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from sqlalchemy import and_
+from sqlalchemy import and_, func
 from nextgisweb import DBSession
 from nextgisweb.resource import Widget, Resource
 from nextgisweb import dynmenu as dm
 from nextgisweb.pyramid import viewargs
-from .model import FoclProject, FoclStruct, SituationPlan, PROJECT_STATUS_PROJECT, \
-    PROJECT_STATUS_IN_PROGRESS, PROJECT_STATUS_BUILT, PROJECT_STATUS_DELIVERED, Region, District
+from pyramid.response import Response
+from .model import Region
+import re
+import json
+import transaction
 
 
 def add_routes(config):
     config.add_route(
         'compulink_admin.reference_books.region',
         '/compulink/admin/reference_books/region').add_view(get_region_page)
+    config.add_route(
+        'compulink_admin.services.reference_books.region.get',
+        'compulink/services/reference_books/region/').add_view(get_regions)
+    config.add_route(
+        'compulink_admin.services.reference_books.region.get_one',
+        'compulink/services/reference_books/region/{id}').add_view(get_region)
     config.add_route(
         'compulink_admin.reference_books.district',
         '/compulink/admin/reference_books/district').add_view(get_district_page)
@@ -24,11 +33,130 @@ def add_routes(config):
         '/compulink/admin/reference_books/construct_object').add_view(get_construct_object_page)
 
 
+regions_data_grid = [
+    {
+        'data-property': 'id',
+        'grid-property': 'id',
+        'name': 'Номер',
+        'cell-prop': {
+            'width': '20%'
+        }
+    },
+    {
+        'data-property': 'name',
+        'grid-property': 'name',
+        'name': 'Название',
+        'cell-prop': {
+            'width': '20%'
+        }
+    },
+    {
+        'data-property': 'short_name',
+        'grid-property': 'short_name',
+        'name': 'Краткое название',
+        'cell-prop': {
+            'width': '20%'
+        }
+    },
+    {
+        'data-property': 'region_code',
+        'grid-property': 'region_code',
+        'name': 'Код региона',
+        'cell-prop': {
+            'width': '20%'
+        }
+    }
+]
+
+
 @viewargs(renderer='nextgisweb_compulink:compulink_admin/template/reference_books/regions.mako')
 def get_region_page(request):
+    grid_config_store = []
+    for config_item in regions_data_grid:
+        grid_config_store_item = {
+            'name': config_item['name'],
+            'field': config_item['grid-property']
+        }
+        grid_config_store_item.update(config_item['cell-prop'])
+        grid_config_store.append(grid_config_store_item)
+
     return {
-        'title': u'Справочник регионов'
+        'title': u'Справочник регионов',
+        'gridConfigStore': grid_config_store,
+        'dynmenu': request.env.pyramid.control_panel
     }
+
+
+@viewargs(renderer='json')
+def get_regions(request):
+    session = DBSession()
+
+    regions = session.query(Region)
+
+    sort_keys = filter(lambda k: 'sort(' in k, request.GET.keys())
+    if sort_keys:
+        dojo_order, grid_field_name = re.findall('.+([+,-])(\w+)', request.query_string)[0]
+        field_name = filter(lambda x: x['grid-property'] == grid_field_name, regions_data_grid)[0]['data-property']
+        if dojo_order == '+':
+            regions = regions.order_by(Region.__table__.c[field_name].asc())
+        elif dojo_order == '-':
+            regions = regions.order_by(Region.__table__.c[field_name].desc())
+
+    grid_range = None
+    if 'Range' in request.headers:
+        grid_range = request.headers['Range']
+        start, stop = re.findall('items=(\d+)-(\d+)', grid_range)[0]
+        regions = regions.slice(int(start), int(stop) + 1)
+
+    result = []
+    for region in regions:
+        result_item = {}
+        for item_config in regions_data_grid:
+            result_item[item_config['grid-property']] = region.__getattribute__(item_config['data-property'])
+        result.append(result_item)
+
+    response = Response(json.dumps(result))
+
+    if grid_range:
+        count_all_rows = session.query(func.count(Region.id)).scalar()
+        response.headers[str('Content-Range')] = str(grid_range + '/' + str(count_all_rows))
+
+    return response
+
+
+@viewargs(renderer='json')
+def get_region(request):
+    if request.method == 'PUT':
+        return put_region(request)
+
+    region_id = request.matchdict['id']
+
+    if region_id == 'undefined':
+        return get_regions(request)
+
+    session = DBSession()
+    region = session.query(Region).filter(Region.id == region_id).one()
+
+    result = {}
+    for item_config in regions_data_grid:
+        result[item_config['grid-property']] = region.__getattribute__(item_config['data-property'])
+
+    return Response(json.dumps(result))
+
+
+@viewargs(renderer='json')
+def put_region(request):
+    region_id = request.matchdict['id']
+
+    item_updatable = {}
+    for item_config in regions_data_grid:
+        item_updatable[item_config['data-property']] = request.json[item_config['grid-property']]
+
+    session = DBSession()
+    session.query(Region).filter(Region.id == region_id).update(item_updatable)
+    transaction.commit()
+
+    return Response(json.dumps(item_updatable))
 
 
 @viewargs(renderer='nextgisweb_compulink:compulink_admin/template/reference_books/districts.mako')
