ddf5a272c1428a56c807ed10e80041902432b6bb
Ivan Kavaliou <jownkiv@gmail.com>
2016-01-04 14:41:17 +0300

    #276 reference_books routes register was moved to routes.initialize func

---
+++ nextgisweb_compulink/compulink_admin/reference_books/routes.py
@@ -0,0 +1,22 @@
+from views import pages, regions
+
+
+def initialize(config):
+    config.add_route(
+        'compulink_admin.reference_books.region',
+        '/compulink/admin/reference_books/region').add_view(pages.get_region_page)
+    config.add_route(
+        'compulink_admin.services.reference_books.region.get',
+        'compulink/services/reference_books/region/').add_view(regions.get_regions)
+    config.add_route(
+        'compulink_admin.services.reference_books.region.get_one',
+        'compulink/services/reference_books/region/{id}').add_view(regions.get_region)
+    config.add_route(
+        'compulink_admin.reference_books.district',
+        '/compulink/admin/reference_books/district').add_view(pages.get_district_page)
+    config.add_route(
+        'compulink_admin.reference_books.project',
+        '/compulink/admin/reference_books/project').add_view(pages.get_project_page)
+    config.add_route(
+        'compulink_admin.reference_books.construct_object',
+        '/compulink/admin/reference_books/construct_object').add_view(pages.get_construct_object_page)
