19779081c20480a17ba0dec04cd13da4b5ed985b
Ivan Kavaliou <jownkiv@gmail.com>
2016-01-08 17:38:15 +0300

    #280 _get_items method was implemented

--- nextgisweb_compulink/compulink_admin/reference_books/views/ReferenceBookViewBase.py
+++ nextgisweb_compulink/compulink_admin/reference_books/views/ReferenceBookViewBase.py
@@ -33,3 +33,60 @@ class ReferenceBookViewBase(object):
         }
 
         return render_to_response(template, view_data, request=self.request)
+
+    def _get_items(self, reference_book_type, dgrid_viewmodel):
+        session = DBSession()
+
+        items_query = session.query(reference_book_type)
+
+        rel_attrs = filter(lambda c: 'relation' in c, dgrid_viewmodel)
+        for rel_attr in rel_attrs:
+            relation_field = rel_attr['relation']['relation-field']
+            items_query = items_query.outerjoin(relation_field).options(joinedload(relation_field))
+
+        sort_keys = filter(lambda k: 'sort(' in k, self.request.GET.keys())
+        if sort_keys:
+            dojo_order, grid_field_name = re.findall('.+([+,-])(\w+)', self.request.query_string)[0]
+            item_config = filter(lambda x: x['grid-property'] == grid_field_name, dgrid_viewmodel)[0]
+            if 'relation' in item_config:
+                sorting_field = item_config['relation']['sort-field']
+            else:
+                field_name = item_config['data-property']
+                sorting_field = reference_book_type.__table__.c[field_name]
+
+            if dojo_order == '+':
+                items_query = items_query.order_by(sorting_field.asc())
+            elif dojo_order == '-':
+                items_query = items_query.order_by(sorting_field.desc())
+
+        grid_range = None
+        if 'Range' in self.request.headers:
+            grid_range = self.request.headers['Range']
+            start, stop = re.findall('items=(\d+)-(\d+)', grid_range)[0]
+            items_query = items_query.slice(int(start), int(stop) + 1)
+
+        result = []
+        for item_query in items_query:
+            result_item = {}
+            for item_config in dgrid_viewmodel:
+                if 'relation' in item_config:
+                    rel_attr_name = item_config['data-property']
+                    rel_attr = item_query.__getattribute__(rel_attr_name)
+                    if rel_attr:
+                        result_item[rel_attr_name] = rel_attr.__getattribute__(item_config['relation']['label'])
+                        result_item[rel_attr_name + '_id'] = rel_attr.__getattribute__(item_config['relation']['id'])
+                    else:
+                        result_item[rel_attr_name] = None
+                        result_item[rel_attr_name + '_id'] = None
+                else:
+                    result_item[item_config['grid-property']] = \
+                        item_query.__getattribute__(item_config['data-property'])
+            result.append(result_item)
+
+        response = Response(json.dumps(result))
+
+        if grid_range:
+            count_all_rows = session.query(func.count(reference_book_type.id)).scalar()
+            response.headers[str('Content-Range')] = str(grid_range + '/' + str(count_all_rows))
+
+        return response
