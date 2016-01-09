5105aa8c63c7c5b566422a56cd5e6beb6b1c3aac
Ivan Kavaliou <jownkiv@gmail.com>
2016-01-09 20:28:28 +0300

    #281 EditorRelation prototype was added

---
+++ nextgisweb_compulink/compulink_admin/amd/ngw-compulink-admin/reference_books/EditorRelation.js
@@ -0,0 +1,12 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/topic',
+    'dojo/on',
+    'dojo/_base/array',
+    'ngw-compulink-libs/dgrid-0.4.0/dgrid/Editor'
+], function (declare, lang, topic, on, array, Editor) {
+    return declare([Editor], {
+
+    });
+});
