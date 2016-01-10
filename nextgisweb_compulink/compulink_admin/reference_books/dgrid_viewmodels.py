06a2ace1e106d1f60c0fc5e160e1b4fb4062b052
Ivan Kavaliou <jownkiv@gmail.com>
2016-01-10 17:44:40 +0300

    #280 projects_dgrid_viewmodel prototype was added to dgrid_viewmodel

--- nextgisweb_compulink/compulink_admin/reference_books/dgrid_viewmodels.py
+++ nextgisweb_compulink/compulink_admin/reference_books/dgrid_viewmodels.py
@@ -94,3 +94,71 @@ districts_dgrid_viewmodel = [
     }
 ]
 
+projects_dgrid_viewmodel = [
+    {
+        'data-property': 'id',
+        'grid-property': 'id',
+        'id': True,
+        'label': 'Идентификатор',
+        'cell-prop': {
+
+        }
+    },
+    {
+        'data-property': 'name',
+        'grid-property': 'name',
+        'label': 'Название',
+        'cell-prop': {
+            'editor': 'text',
+            'editOn': 'dblclick',
+            'autoSave': True
+        }
+    },
+    {
+        'data-property': 'short_name',
+        'grid-property': 'short_name',
+        'label': 'Краткое название',
+        'cell-prop': {
+            'editor': 'text',
+            'editOn': 'dblclick',
+            'autoSave': True
+        }
+    },
+    {
+        'data-property': 'description',
+        'grid-property': 'description',
+        'label': 'Описание',
+        'cell-prop': {
+            'editor': 'text',
+            'editOn': 'dblclick',
+            'autoSave': True
+        }
+    },
+    {
+        'data-property': 'keyname',
+        'grid-property': 'keyname',
+        'label': 'Ключ',
+        'cell-prop': {
+            'editor': 'text',
+            'editOn': 'dblclick',
+            'autoSave': True
+        }
+    },
+    {
+        'data-property': 'root_resource',
+        'grid-property': 'root_resource',
+        'relation': {
+            'id': 'id',
+            'label': 'display_name',
+            'relation-field': Project.root_resource,
+            'sort-field': Resource.display_name,
+            'type': Resource
+        },
+        'label': 'Регион',
+        'cell-prop': {
+            'editor': 'widget=>RelationSelect',
+            'editOn': 'dblclick',
+            'autoSave': True
+        }
+    }
+]
