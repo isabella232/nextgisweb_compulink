97996e0357afc365ec679448533a4188293fbc69
Ivan Kavaliou <jownkiv@gmail.com>
2016-01-12 14:38:13 +0300

    closed #286 date type was added

--- nextgisweb_compulink/compulink_reporting/utils.py
+++ nextgisweb_compulink/compulink_reporting/utils.py
@@ -1,13 +1,13 @@
 __author__ = 'yellow'
 
 from datetime import datetime
+from datetime import date
 import json
 
 
 class DateTimeJSONEncoder(json.JSONEncoder):
-
     def default(self, obj):
-        if isinstance(obj, datetime):
+        if isinstance(obj, datetime) or isinstance(obj, date):
             return obj.isoformat()
 
         return super(DateTimeJSONEncoder, self).default(obj)
