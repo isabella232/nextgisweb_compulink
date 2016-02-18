8840a7cc891ba5aaa567f599e7bfd7ea0e62277a
yellow-sky <nikulin.e@gmail.com>
2016-02-18 18:44:43 +0300

    Resolve #330

--- nextgisweb_compulink/compulink_editor/config.py
+++ nextgisweb_compulink/compulink_editor/config.py
@@ -2,7 +2,7 @@ EDITABLE_LAYERS = {
     'actual_real_special_transition': {
         'style': {
             'strokeColor': '#ff3300',
-            'pointRadius': 10,
+            'pointRadius': 6,
             'fillColor': '#ff3300',
             'strokeWidth': 5,
             'stroke': True,
@@ -13,7 +13,7 @@ EDITABLE_LAYERS = {
     'actual_real_special_transition_point': {
         'style': {
             'strokeColor': '#ff3300',
-            'pointRadius': 13,
+            'pointRadius': 6,
             'fillColor': '#ff3300',
             'strokeWidth': 2,
             'stroke': True,
@@ -24,7 +24,7 @@ EDITABLE_LAYERS = {
     'actual_real_optical_cable': {
         'style': {
             'strokeColor': '#0066ff',
-            'pointRadius': 10,
+            'pointRadius': 6,
             'fillColor': '#0066ff',
             'strokeWidth': 3,
             'stroke': True,
@@ -35,18 +35,19 @@ EDITABLE_LAYERS = {
     'actual_real_optical_cable_point': {
         'style': {
             'strokeColor': '#0066ff',
-            'pointRadius': 10,
+            'pointRadius': 6,
             'fillColor': '#0066ff',
             'strokeWidth': 1,
             'stroke': True,
             'fill': True,
             'fillOpacity': 0.6
+            # 'graphicName': 'cross'
         }
     },
     'actual_real_fosc': {
         'style': {
             'strokeColor': '#ff00ff',
-            'pointRadius': 10,
+            'pointRadius': 6,
             'fillColor': '#ff00ff',
             'strokeWidth': 3,
             'stroke': True,
@@ -57,7 +58,7 @@ EDITABLE_LAYERS = {
     'actual_real_optical_cross': {
         'style': {
             'strokeColor': '#ff00ff',
-            'pointRadius': 10,
+            'pointRadius': 6,
             'fillColor': '#ff00ff',
             'strokeWidth': 1,
             'stroke': True,
@@ -68,7 +69,7 @@ EDITABLE_LAYERS = {
     'actual_real_access_point': {
         'style': {
             'strokeColor': '#ff00ff',
-            'pointRadius': 10,
+            'pointRadius': 6,
             'fillColor': '#ff00ff',
             'strokeWidth': 1,
             'stroke': True,
