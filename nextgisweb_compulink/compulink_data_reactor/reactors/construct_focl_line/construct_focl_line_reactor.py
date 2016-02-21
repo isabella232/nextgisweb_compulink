1fa4000e7f07b3f5ee4fc3a28b028c47173a0842
yellow-sky <nikulin.e@gmail.com>
2016-02-21 17:26:56 +0300

    Small refactoring

--- nextgisweb_compulink/compulink_data_reactor/reactors/construct_focl_line/construct_focl_line_reactor.py
+++ nextgisweb_compulink/compulink_data_reactor/reactors/construct_focl_line/construct_focl_line_reactor.py
@@ -30,83 +30,85 @@ class ConstructFoclLineReactor(AbstractReactor):
     # Max distance of point in segment
     DISTANCE_LIMIT = 350
 
+    log_info = staticmethod(lambda x: LogEntry.info(x, component=COMP_ID, group=ConstructFoclLineReactor.identity, append_dt=datetime.now()))
+    log_debug = staticmethod(lambda x: LogEntry.debug(x, component=COMP_ID, group=ConstructFoclLineReactor.identity, append_dt=datetime.now()))
+    log_warning = staticmethod(lambda x: LogEntry.warning(x, component=COMP_ID, group=ConstructFoclLineReactor.identity, append_dt=datetime.now()))
+
     @classmethod
     def run(cls, env):
-
-        log_info = lambda x: LogEntry.info(x, component=COMP_ID, group=ConstructFoclLineReactor.identity, append_dt=datetime.now())
-        log_debug = lambda x: LogEntry.debug(x, component=COMP_ID, group=ConstructFoclLineReactor.identity, append_dt=datetime.now())
-        log_warning = lambda x: LogEntry.warning(x, component=COMP_ID, group=ConstructFoclLineReactor.identity, append_dt=datetime.now())
 
         db_session = DBSession()
         transaction.manager.begin()
 
-        log_info('ConstructFoclLineReactor started! (%s)' % CONNECTOR_NAME)
+        cls.log_info('ConstructFoclLineReactor started! (%s)' % CONNECTOR_NAME)
 
         fs_resources = db_session.query(FoclStruct).all()
         for fs in fs_resources:
-
-            # Output layer
-            lines_lyr = [lyr for lyr in fs.children if lyr.keyname and
-                         not lyr.keyname.startswith('actual_real_optical_cable_point') and
-                         lyr.keyname.startswith('actual_real_optical_cable')]
-            lines_lyr = lines_lyr[0] if len(lines_lyr) else None
-
-
-            # Collect features for processing
-            features = []
-            processing_layers_name = ['actual_real_optical_cable_point',
-                                      'actual_real_special_transition_point',
-                                      'actual_real_fosc',
-                                      'actual_real_optical_cross',
-                                      'actual_real_access_point']
-
-            for lyr_name in processing_layers_name:
-                points_lyr = [lyr for lyr in fs.children if lyr.keyname and lyr.keyname.startswith(lyr_name)]
-                points_lyr = points_lyr[0] if len(points_lyr) else None
-
-                if points_lyr:
-                    query = points_lyr.feature_query()
-                    query.geom()
-                    result = query()
-                    features.extend([feature for feature in result])
-
-
-            if len(features) > 0:
-                log_debug('Construct line for %s started!' % fs.display_name)
-            else:
-                log_debug('Construct line for %s skeeped (no points)!' % fs.display_name)
-                continue
-
-            #clear line lyr
-            cls.clear_layer(lines_lyr)
-
-            #get clusters
-            clusters = cls.get_clusters(features)
-
-            #merge points in clusters
-            for cluster in clusters:
-                if len(cluster) < 2:
-                    log_warning('Line %s has unclustered point!')
-                    continue
-                if len(cluster) == 2:
-                    # construct segment
-                    points = tuple(feat.geom[0].coords[0] for feat in cluster)
-                    # write segment
-                    info = cls.get_segment_info(points, cluster)
-                    cls.write_segment(lines_lyr, points, cluster, info)
-                if len(cluster) > 2:
-                    line = cls.make_line(cluster)
-                    # write segments
-                    for i in range(len(line[0])-1):
-                        points = (line[0][i], line[0][i+1])
-                        info = cls.get_segment_info(points, cluster)
-                        cls.write_segment(lines_lyr, points, cluster, info)
-
+            cls.construct_line(fs)
             db_session.flush()
 
-        log_info('ConstructFoclLineReactor finished!')
-
+        cls.log_info('ConstructFoclLineReactor finished!')
         transaction.manager.commit()
+
+
+    @classmethod
+    def construct_line(cls, focl_struct_resource):
+        # Output layer
+        lines_lyr = [lyr for lyr in focl_struct_resource.children if lyr.keyname and
+                     not lyr.keyname.startswith('actual_real_optical_cable_point') and
+                     lyr.keyname.startswith('actual_real_optical_cable')]
+        lines_lyr = lines_lyr[0] if len(lines_lyr) else None
+
+
+        # Collect features for processing
+        features = []
+        processing_layers_name = ['actual_real_optical_cable_point',
+                                  'actual_real_special_transition_point',
+                                  'actual_real_fosc',
+                                  'actual_real_optical_cross',
+                                  'actual_real_access_point']
+
+        for lyr_name in processing_layers_name:
+            points_lyr = [lyr for lyr in focl_struct_resource.children if lyr.keyname and lyr.keyname.startswith(lyr_name)]
+            points_lyr = points_lyr[0] if len(points_lyr) else None
+
+            if points_lyr:
+                query = points_lyr.feature_query()
+                query.geom()
+                result = query()
+                features.extend([feature for feature in result])
+
+
+        if len(features) > 0:
+            cls.log_debug('Construct line for %s started!' % focl_struct_resource.display_name)
+        else:
+            cls.log_debug('Construct line for %s skeeped (no points)!' % focl_struct_resource.display_name)
+            return
+
+        #clear line lyr
+        cls.clear_layer(lines_lyr)
+
+        #get clusters
+        clusters = cls.get_clusters(features)
+
+        #merge points in clusters
+        for cluster in clusters:
+            if len(cluster) < 2:
+                cls.log_warning('Line %s has unclustered point!')
+                continue
+            if len(cluster) == 2:
+                # construct segment
+                points = tuple(feat.geom[0].coords[0] for feat in cluster)
+                # write segment
+                info = cls.get_segment_info(points, cluster)
+                cls.write_segment(lines_lyr, points, cluster, info)
+            if len(cluster) > 2:
+                line = cls.make_line(cluster)
+                # write segments
+                for i in range(len(line[0])-1):
+                    points = (line[0][i], line[0][i+1])
+                    info = cls.get_segment_info(points, cluster)
+                    cls.write_segment(lines_lyr, points, cluster, info)
 
 
 
--- nextgisweb_compulink/compulink_data_reactor/reactors/construct_spec_transition_line/construct_spec_transition_line_reactor.py
+++ nextgisweb_compulink/compulink_data_reactor/reactors/construct_spec_transition_line/construct_spec_transition_line_reactor.py
@@ -21,85 +21,90 @@ class ConstructSpecTransitionLineReactor(AbstractReactor):
     # Max len of spec transition
     DISTANCE_LIMIT = 300
 
+    log_info = staticmethod(lambda x: LogEntry.info(x, component=COMP_ID, group=ConstructSpecTransitionLineReactor.identity, append_dt=datetime.now()))
+    log_debug = staticmethod(lambda x: LogEntry.debug(x, component=COMP_ID, group=ConstructSpecTransitionLineReactor.identity, append_dt=datetime.now()))
+    log_warning = staticmethod(lambda x: LogEntry.warning(x, component=COMP_ID, group=ConstructSpecTransitionLineReactor.identity, append_dt=datetime.now()))
+
+
     @classmethod
     def run(cls, env):
-
-        log_info = lambda x: LogEntry.info(x, component=COMP_ID, group=ConstructSpecTransitionLineReactor.identity, append_dt=datetime.now())
-        log_debug = lambda x: LogEntry.debug(x, component=COMP_ID, group=ConstructSpecTransitionLineReactor.identity, append_dt=datetime.now())
-        log_warning = lambda x: LogEntry.warning(x, component=COMP_ID, group=ConstructSpecTransitionLineReactor.identity, append_dt=datetime.now())
 
         db_session = DBSession()
         transaction.manager.begin()
 
-        log_info('ConstructSpecTransitionLineReactor started!')
+        cls.log_info('ConstructSpecTransitionLineReactor started!')
 
         fs_resources = db_session.query(FoclStruct).all()
         for fs in fs_resources:
-            points_lyr = [lyr for lyr in fs.children if lyr.keyname and lyr.keyname.startswith('actual_real_special_transition_point')]
-            points_lyr = points_lyr[0] if len(points_lyr) else None
-
-            lines_lyr = [lyr for lyr in fs.children if lyr.keyname and
-                         not lyr.keyname.startswith('actual_real_special_transition_point') and
-                         lyr.keyname.startswith('actual_real_special_transition')]
-            lines_lyr = lines_lyr[0] if len(lines_lyr) else None
-
-            query = points_lyr.feature_query()
-            query.geom()
-            result = query()
-
-            if result.total_count > 0:
-                log_debug('Construct spec_trans line for %s started!' % fs.display_name)
-            else:
-                log_debug('Construct spec_trans line for %s skeeped (no points)!' % fs.display_name)
-                continue
-
-            starts = []
-            ends = []
-            for feature in result:
-                if feature.fields['special_laying_number'] == 'entrance':
-                    starts.append(feature)
-                else:
-                    ends.append(feature)
-
-            if len(starts) != len(ends):
-                log_warning('Line %s has unpaired count of start\end points! (%s\%s)' % (fs.display_name, len(starts), len(ends)))
-
-            #clear line lyr
-            cls.clear_layer(lines_lyr)
-
-            #merge points in two mass
-            for start_point_feat in starts:
-                if len(ends) < 1:
-                    continue
-
-                # get near point
-                near_point_feat = ends[0]
-                near_len = start_point_feat.geom.distance(near_point_feat.geom)
-                for end_point_feat in ends:
-                    if start_point_feat.geom.distance(end_point_feat.geom) < near_len:
-                        near_point_feat = end_point_feat
-                        near_len = start_point_feat.geom.distance(end_point_feat.geom)
-
-                # check distance limit
-                real_dist = DistanceUtils.get_spherical_distance(start_point_feat.geom[0], near_point_feat.geom[0])
-                if real_dist > cls.DISTANCE_LIMIT:
-                    log_warning('Point %s has no paired points near that maximum distance!' % start_point_feat.id)
-                    continue
-
-                # construct line
-                line_feats = [start_point_feat, near_point_feat]
-                info = cls.get_segment_info(line_feats)
-                cls.write_segment(lines_lyr, line_feats, info)
-
-                # remove from ends
-                ends.remove(near_point_feat)
-
+            cls.construct_line(fs)
             db_session.flush()
 
-        log_info('ConstructSpecTransitionLineReactor finished!')
+        cls.log_info('ConstructSpecTransitionLineReactor finished!')
 
         transaction.manager.commit()
-  
+
+    @classmethod
+    def construct_line(cls, focl_res):
+        points_lyr = [lyr for lyr in focl_res.children if lyr.keyname and lyr.keyname.startswith('actual_real_special_transition_point')]
+        points_lyr = points_lyr[0] if len(points_lyr) else None
+
+        lines_lyr = [lyr for lyr in focl_res.children if lyr.keyname and
+                     not lyr.keyname.startswith('actual_real_special_transition_point') and
+                     lyr.keyname.startswith('actual_real_special_transition')]
+        lines_lyr = lines_lyr[0] if len(lines_lyr) else None
+
+        query = points_lyr.feature_query()
+        query.geom()
+        result = query()
+
+        if result.total_count > 0:
+            cls.log_debug('Construct spec_trans line for %s started!' % focl_res.display_name)
+        else:
+            cls.log_debug('Construct spec_trans line for %s skeeped (no points)!' % focl_res.display_name)
+            return
+
+        starts = []
+        ends = []
+        for feature in result:
+            if feature.fields['special_laying_number'] == 'entrance':
+                starts.append(feature)
+            else:
+                ends.append(feature)
+
+        if len(starts) != len(ends):
+            cls.log_warning('Line %s has unpaired count of start\end points! (%s\%s)' % (focl_res.display_name, len(starts), len(ends)))
+
+        #clear line lyr
+        cls.clear_layer(lines_lyr)
+
+        #merge points in two mass
+        for start_point_feat in starts:
+            if len(ends) < 1:
+                continue
+
+            # get near point
+            near_point_feat = ends[0]
+            near_len = start_point_feat.geom.distance(near_point_feat.geom)
+            for end_point_feat in ends:
+                if start_point_feat.geom.distance(end_point_feat.geom) < near_len:
+                    near_point_feat = end_point_feat
+                    near_len = start_point_feat.geom.distance(end_point_feat.geom)
+
+            # check distance limit
+            real_dist = DistanceUtils.get_spherical_distance(start_point_feat.geom[0], near_point_feat.geom[0])
+            if real_dist > cls.DISTANCE_LIMIT:
+                cls.log_warning('Point %s has no paired points near that maximum distance!' % start_point_feat.id)
+                continue
+
+            # construct line
+            line_feats = [start_point_feat, near_point_feat]
+            info = cls.get_segment_info(line_feats)
+            cls.write_segment(lines_lyr, line_feats, info)
+
+            # remove from ends
+            ends.remove(near_point_feat)
+
+
     @classmethod
     def clear_layer(cls, layer):
         tableinfo = TableInfo.from_layer(layer)
