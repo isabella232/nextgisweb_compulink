9b106fed695dc2ea6ceb06e05c32f86a7ee00272
Ivan Kavaliou <jownkiv@gmail.com>
2016-03-25 13:44:09 +0300

    closed #383 new order was applied to main map

--- nextgisweb_compulink/compulink_admin/layers_struct_group.py
+++ nextgisweb_compulink/compulink_admin/layers_struct_group.py
@@ -3,20 +3,9 @@
 # относительно того как они будут в толстом клиенте
 
 FOCL_LAYER_STRUCT = [
-    #'other_polygon_object',
-    #'other_line_object',
-    #'gas_pipeline',
-    #'oil_pipeline',
-    #'overhead_power_line',
-    #'other_point_object',
     'note',
     'special_transition',
-    #'cellular_station',
-    #'communication_center',
-    #'electrical_substation',
-    #'telecom_cabinet',
     'optical_cable',
-    #'pole',
     'fosc',
     'optical_cross',
     'access_point',
@@ -30,11 +19,9 @@ OBJECTS_LAYER_STRUCT = [
 
 FOCL_REAL_LAYER_STRUCT = [
     'real_special_transition',
-    'real_special_transition_point',
-
     'real_optical_cable',
+    'real_special_transition_point',
     'real_optical_cable_point',
-
     'real_fosc',
     'real_optical_cross',
     'real_access_point',
@@ -42,11 +29,9 @@ FOCL_REAL_LAYER_STRUCT = [
 
 ACTUAL_FOCL_REAL_LAYER_STRUCT = [
     'actual_real_special_transition',
-    'actual_real_special_transition_point',
-
     'actual_real_optical_cable',
+    'actual_real_special_transition_point',
     'actual_real_optical_cable_point',
-
     'actual_real_fosc',
     'actual_real_optical_cross',
     'actual_real_access_point',
@@ -55,16 +40,11 @@ ACTUAL_FOCL_REAL_LAYER_STRUCT = [
 
 SIT_PLAN_LAYER_STRUCT = [
     'sp_other_polygon_object',
-    #'sp_boundary',
     'sp_other_line_object',
-    #'sp_gas_pipeline',
-    #'sp_oil_pipeline',
     'sp_overhead_power_line',
     'sp_focl',
     'sp_other_point_object',
     'sp_note',
-    #'sp_photo',
-    #'sp_cellular_station',
     'sp_sump_canalization',
     'sp_communication_center',
     'sp_electrical_substation',
