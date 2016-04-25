ccbb3fe40529f4bc606c9e235cbeb783a54d6890
Ivan Kavaliou <jownkiv@gmail.com>
2016-04-25 15:55:03 +0300

    #384, #385 remove/add photo was implemented to Identify

--- nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/tool/attachments/FotoDisplayWidget.js
+++ nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/tool/attachments/FotoDisplayWidget.js
@@ -1,25 +1,47 @@
 define([
     "dojo/_base/declare",
     "dojo/_base/array",
+    "dojo/_base/lang",
     "dojo/dom-class",
+    "dojo/dom-construct",
+    "dojo/query",
     "dojo/on",
+    "dojo/html",
+    "dojo/aspect",
+    "dojo/request/xhr",
     "dojox/image/Lightbox",
+    "dojox/form/Uploader",
+    "dojox/dtl",
+    "dojox/dtl/Context",
+    "dojox/dtl/_base",
     "put-selector/put",
     "ngw/route",
     "ngw-feature-layer/DisplayWidget",
-    // css
+    "dojo/text!./resource/FotoDisplayWidget.html",
+    "dojo/text!./resource/FotoDisplayWidgetItem.html",
+    "dojo/NodeList-traverse",
     "xstyle/css!" + ngwConfig.amdUrl + "dojox/image/resources/Lightbox.css",
     "xstyle/css!./resource/DisplayWidget.css"
-], function (
-    declare,
-    array,
-    domClass,
-    on,
-    Lightbox,
-    put,
-    route,
-    DisplayWidget
-) {
+], function (declare,
+             array,
+             lang,
+             domClass,
+             domConstruct,
+             query,
+             on,
+             html,
+             aspect,
+             xhr,
+             Lightbox,
+             Uploader,
+             dtl,
+             dtlContext,
+             dtlBase,
+             put,
+             route,
+             DisplayWidget,
+             template,
+             templateItem) {
     function fileSizeToString(size) {
         var units = ["B", "KB", "MB", "GB"],
             i = 0;
@@ -34,7 +56,54 @@ define([
         title: "Прикрепленные файлы",
 
         constructor: function (args) {
-            if (args.compact === true) { this.title = "Фото"; }
+            if (args.compact === true) {
+                this.title = "Фото";
+            }
+
+            aspect.after(this, "placeAt", lang.hitch(this, function (deferred) {
+                var uploader = new Uploader({
+                    label: "Загрузить",
+                    iconClass: "dijitIconNewTask",
+                    multiple: true,
+                    uploadOnSelect: true,
+                    url: ngwConfig.applicationUrl + "/api/component/file_upload/upload",
+                    name: 'file'
+                }).placeAt(query("div.panel", this.domNode)[0]);
+
+                uploader.on("complete", lang.hitch(this, function (data) {
+                    array.forEach(data.upload_meta, function (f) {
+                        var putUrlTmpl = new dtlBase.Template("/api/resource/{{id}}/feature/{{fid}}/attachment/", true),
+                            putUrlContext = new dtlBase.Context({
+                                id: this.resourceId,
+                                fid: this.featureId,
+                                aid: f.id
+                            }),
+                            putUrl = putUrlTmpl.render(putUrlContext);
+
+                        xhr(ngwConfig.applicationUrl + putUrl, {
+                            handleAs: "json",
+                            method: "POST",
+                            data: JSON.stringify({
+                                file_upload: {
+                                    id: f.id,
+                                    size: f.size
+                                },
+                                mime_type: f.mime_type,
+                                name: f.name,
+                                size: f.size
+                            })
+                        }).then(lang.hitch(this, function (data) {
+                            this.renderImage(this.resourceId, this.featureId, {
+                                id: data.id,
+                                mime_type: f.mime_type,
+                                name: f.name,
+                                size: f.size
+                            });
+                            this.updateTitle();
+                        }));
+                    }, this);
+                }));
+            }));
         },
 
         buildRendering: function () {
@@ -53,37 +122,82 @@ define([
         renderValue: function (value) {
             var images = [], others = [];
             array.forEach(value, function (i) {
-                if (i.is_image) { images.push(i); }
-                else { others.push(i); }
+                if (i.is_image) {
+                    images.push(i);
+                }
+                else {
+                    others.push(i);
+                }
             });
 
-            this.title = "Фото [" + images.length.toString() + "]";
+            var tmplWrapper = new dtlBase.Template(template);
+            domConstruct.place(tmplWrapper.render(), this.domNode, "last");
 
             if (images.length > 0) {
                 array.forEach(images, function (image) {
-                    var href = route.feature_attachment.download({
-                        id: this.resourceId,
-                        fid: this.featureId,
-                        aid: image.id
-                    });
-
-                    var src = route.feature_attachment.image({
-                        id: this.resourceId,
-                        fid: this.featureId,
-                        aid: image.id
-                    }) + (this.compact ? "?size=64x64" : "?size=128x128");
-
-                    var a = put(this.domNode, "a.image[href=$] img[src=$] <", href, src);
-
-                    var lbox = this.lbox;
-                    lbox.addImage({href: href, title: image.description || image.name}, "main");
-
-                    on(a, "click", function (evt) {
-                        lbox.show({group: "main", href: href, title: image.description || image.name});
-                        evt.preventDefault();
-                    });
+                    this.renderImage(this.resourceId, this.featureId, image);
                 }, this);
             }
+
+            this.title = 'Фото &#91;<span id="photoCount">' + query('div.image', this.domNode).length + '</span>&#93;';
+        },
+
+        updateTitle: function () {
+            html.set(document.getElementById('photoCount'), query('div.image', this.domNode).length.toString());
+        },
+
+        renderImage: function (resourceId, fid, image) {
+            var aid = image.id;
+
+            var href = route.feature_attachment.download({
+                id: resourceId,
+                fid: fid,
+                aid: aid
+            });
+
+            var src = route.feature_attachment.image({
+                    id: resourceId,
+                    fid: fid,
+                    aid: aid
+                }) + (this.compact ? "?size=64x64" : "?size=128x128");
+
+            var tmpl = new dtl.Template(templateItem);
+            var context = new dtlContext({
+                title: image.name,
+                url: src
+            });
+            var imageElement = domConstruct.place(tmpl.render(context),
+                query("div.photo-content", this.domNode)[0],
+                "last");
+
+            var lbox = this.lbox;
+            lbox.addImage({href: href, title: image.description || image.name}, "main");
+
+            on(query("div.imgWrapper", imageElement), "click", function (evt) {
+                lbox.show({group: "main", href: href, title: image.description || image.name});
+                evt.preventDefault();
+            });
+
+            on(query("div.remove", imageElement), "click", lang.hitch(this, function (evt) {
+                var removeUrlTmpl = new dtlBase.Template("/api/resource/{{id}}/feature/{{fid}}/attachment/{{aid}}", true),
+                    removeUrlContext = new dtlBase.Context({
+                        id: resourceId,
+                        fid: fid,
+                        aid: aid
+                    }),
+                    removeUrl = removeUrlTmpl.render(removeUrlContext),
+                    imageElement = evt.target.parentElement;
+
+                xhr(ngwConfig.applicationUrl + removeUrl, {
+                    handleAs: "json",
+                    method: "DELETE"
+                }).then(lang.hitch(this, function (data) {
+                    domConstruct.destroy(imageElement);
+                    this.updateTitle();
+                }));
+
+                evt.preventDefault();
+            }));
         }
     });
 });
