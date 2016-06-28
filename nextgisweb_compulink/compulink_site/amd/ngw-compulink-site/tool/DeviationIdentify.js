e14f41f8c6df95529d7f51327383acb22b6167a3
Ivan Kavaliou <jownkiv@gmail.com>
2016-06-28 22:47:12 +0300

    #415 DeviationIdentify with context menu was implemented

--- nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/tool/DeviationIdentify.js
+++ nextgisweb_compulink/compulink_site/amd/ngw-compulink-site/tool/DeviationIdentify.js
@@ -3,11 +3,17 @@ define([
     'dojo/_base/array',
     'dojo/request/xhr',
     'dojo/_base/lang',
+    'dojo/dom-construct',
     'dojo/json',
+    'dijit/Menu',
+    'dijit/MenuItem',
     'ngw/route',
     'ngw-compulink-site/tool/DeviationIdentifyControl',
-    './Identify'
-], function (declare, array, xhr, lang, json, route, DeviationIdentifyControl, Identify) {
+    './Identify',
+    './IdentifyPopup'
+], function (declare, array, xhr, lang, domConstruct, json,
+             Menu, MenuItem, route, DeviationIdentifyControl,
+             Identify, IdentifyPopup) {
     return declare(Identify, {
         constructor: function () {
             this.inherited(arguments);
@@ -31,43 +37,66 @@ define([
         rightClickDeviation: function (pixel) {
             var olMap = this.display.map.olMap,
                 point = olMap.getLonLatFromPixel(pixel),
-                vectors_ids = [],
-                request = {
-                    srs: 3857,
-                    geom: this._requestGeomString([pixel.x, pixel.y]),
-                    layers: []
-                },
+                request,
+                layersInfoIdentify,
+                layersLabels,
                 isIdentifyActive = true;
 
             this._addIdentifyLoadingMarker(point);
 
-            for (var lyr_name in this.display.map.layers) {
-                var lyr = this.display.map.layers[lyr_name];
-                if (lyr.vectors_ids) {
-                    vectors_ids = lyr.vectors_ids.split(',');
-                    array.forEach(vectors_ids, function (vector_id, index) {
-                        request.layers.push(vector_id);
-                    }, this);
-                }
-            }
+            layersInfoIdentify = this._getLayersInfoIdentify();
+            layersLabels = this._getLayersLabel(layersInfoIdentify);
 
-            var layerLabels = {};
-            array.forEach(request.layers, function (i) {
-                layerLabels[i] = i;
-            }, this);
+            request = {
+                srs: 3857,
+                geom: this._requestGeomString([pixel.x, pixel.y]),
+                layers: layersInfoIdentify
+            };
 
             xhr.post(route("compulink.deviation.identify"), {
                 handleAs: "json",
                 data: json.stringify(request)
             }).then(lang.hitch(this, function (response) {
-                console.log(response);
                 isIdentifyActive = false;
                 this._clearIdentifyLayer();
-                this._responsePopup(response, point, layerLabels);
+                var marker = this._addIdentifyMarker(point);
+                this._buildDeviationsMenu(response, layersLabels, marker.icon.imageDiv);
             }), lang.hitch(this, function (err) {
                 console.log(err);
                 this._clearIdentifyLayer();
             }));
+        },
+
+        _buildDeviationsMenu: function (response, layersLabels, div) {
+            var layersOrdered, layerId, layerOrderedItem,
+                layerResponse, idx, node, deviationMenu,
+                menuItemLabel;
+
+            node = domConstruct.create('div', {
+                'class': 'identify-deviation-menu'
+            }, div);
+            deviationMenu = new Menu({}, node);
+
+            layersOrdered = IdentifyPopup.getLayersOrdered(this.layersManager);
+            for (var i = 0, layersCount = layersOrdered.length; i < layersCount; i++) {
+                layerOrderedItem = layersOrdered[i];
+                layerId = layerOrderedItem.id;
+                layerResponse = response[layerId];
+                idx = 0;
+                array.forEach(layerResponse.features, function (feature) {
+                    menuItemLabel = layerOrderedItem.layerName + ': ' +
+                        (feature.label || "#" + feature.id) +
+                        " (" + feature.parent + ")";
+
+                    deviationMenu.addChild(new MenuItem({
+                        label: menuItemLabel
+                    }));
+
+                    idx++;
+                }, this);
+            }
+
+            deviationMenu.startup();
         }
     });
 });
