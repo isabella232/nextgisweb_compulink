cf8c1a3c6a3412047f76c826d69d21fe6d13c4ca
yellow-sky <nikulin.e@gmail.com>
2016-10-21 13:33:40 +0300

    Add new package

--- nextgisweb_compulink/__init__.py
+++ nextgisweb_compulink/__init__.py
@@ -24,6 +24,7 @@ def pkginfo():
             compulink_internal_mirroring='nextgisweb_compulink.compulink_internal_mirroring',
             compulink_statistic_map='nextgisweb_compulink.compulink_statistic_map',
             compulink_deviation='nextgisweb_compulink.compulink_deviation',
+            compulink_accepted_part='nextgisweb_compulink.compulink_accepted_part',
         )
     )
 
@@ -37,5 +38,6 @@ def amd_packages():
         ('ngw-compulink-libs', 'nextgisweb_compulink:compulink_site/static/js'),
         ('ngw-compulink-statistic-map', 'nextgisweb_compulink:compulink_statistic_map/amd/ngw-compulink-statistic-map'),
         ('ngw-compulink-deviation', 'nextgisweb_compulink:compulink_deviation/amd/ngw-compulink-deviation'),
+        #('ngw-compulink-accepted-part', 'nextgisweb_compulink:compulink_accepted_part/amd/ngw-compulink-accepted-part'),
         ('jquery', 'nextgisweb_compulink:compulink_site/static/js/jquery-1.11.2')
     )
