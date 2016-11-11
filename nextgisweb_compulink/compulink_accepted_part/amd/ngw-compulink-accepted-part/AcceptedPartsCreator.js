04f056a9dc3a4bb551e2d5511795bf834e8054ce
Ivan Kavaliou <jownkiv@gmail.com>
2016-11-11 22:03:43 +0300

    #504 verification procedure was added

--- nextgisweb_compulink/compulink_accepted_part/amd/ngw-compulink-accepted-part/AcceptedPartsCreator.js
+++ nextgisweb_compulink/compulink_accepted_part/amd/ngw-compulink-accepted-part/AcceptedPartsCreator.js
@@ -46,9 +46,7 @@ define([
 
             this._drawFeatureControl.handler.callbacks.point = lang.hitch(this, this._createPointSketchHandler);
 
-            aspect.after(this._drawFeatureControl.handler, 'addPoint', lang.hitch(this, function (a) {
-                console.log(a);
-            }));
+            aspect.after(this._drawFeatureControl.handler, 'up', lang.hitch(this, this._afterDrawUpHandler));
 
             this._snappingControl = new openlayers.Control.Snapping({
                 layer: this._acceptedPartsLayer._layer,
@@ -74,12 +72,6 @@ define([
             this._map.olMap.addControl(this._drawFeatureControl);
             this._snappingControl.activate();
             this._drawFeatureControl.activate();
-
-            // this._acceptedPartsLayer._layer.events.register('sketchcomplete',
-            //     this._acceptedPartsLayer._layer, function (a1, a2) {
-            //         console.log(a1);
-            //         console.log(a2);
-            //     });
         },
 
         _deactivate: function () {
@@ -88,26 +80,87 @@ define([
             this._drawFeatureControl.deactivate();
         },
 
+        _lastPointVerified: {
+            result: false,
+            pointsInSketchLine: 0
+        },
+
         _createPointSketchHandler: function (point, sketchLine) {
-            var verifyResult,
-                pointsInSketchLine = sketchLine.components.length;
+            var pointsInSketchLine = sketchLine.components.length;
 
             if (pointsInSketchLine === 2) {
-                verifyResult = this._verifyStartPoint(point, sketchLine);
-                if (!verifyResult) {
-                    this._drawFeatureControl.cancel();
-                }
+                this._lastPointVerifyResult = {
+                    result: this._verifyStartPoint(point, sketchLine),
+                    pointsInSketchLine: pointsInSketchLine
+                };
             }
 
             if (pointsInSketchLine === 3) {
+                this._lastPointVerifyResult = {
+                    result: this._verifyEndPoint(point, sketchLine),
+                    pointsInSketchLine: pointsInSketchLine
+                };
+            }
+        },
 
+        _afterDrawUpHandler: function () {
+            if (this._lastPointVerifyResult.result && this._lastPointVerifyResult.pointsInSketchLine === 2) return true;
+
+            // if start point, then this._lastPointVerifyResult.pointsInSketchLine === 2
+            if (this._lastPointVerifyResult.pointsInSketchLine === 2) {
+                this._drawFeatureControl.cancel();
+                return true;
+            }
+
+            // if this._lastPointVerifyResult.pointsInSketchLine !== 2 then current point is end point
+            if (this._lastPointVerifyResult.result) {
+
+            } else {
+                this._drawFeatureControl.undo();
+                return true;
             }
         },
 
         _verifyStartPoint: function (point, sketchLine) {
             var startPoint = sketchLine.components[0];
-            return this._isPointContainsInLinesLayer(startPoint, this._actualRealOpticalCableLayer._layer) &&
-                !this._isPointContainsInLinesLayer(startPoint, this._acceptedPartsLayer._layer);
+            return (this._isPointContainsInLinesLayer(startPoint, this._actualRealOpticalCableLayer._layer) === true) &&
+                (this._isPointContainsInLinesLayer(startPoint, this._acceptedPartsLayer._layer) === false);
+        },
+
+        _verifyEndPoint: function (point, sketchLine) {
+            var startPoint = sketchLine.components[0],
+                endPoint = sketchLine.components[1],
+                linestring;
+
+            if (this._isPointContainsInLinesLayer(endPoint, this._actualRealOpticalCableLayer._layer) !== true ||
+                this._isPointContainsInLinesLayer(endPoint, this._acceptedPartsLayer._layer) !== false) {
+                return false;
+            }
+
+            linestring = this._pointsOnOneLine(startPoint, endPoint, this._actualRealOpticalCableLayer._layer.features[0].geometry);
+            if (!linestring) {
+                return false;
+            }
+
+            return {
+                startPoint: startPoint,
+                endPoint: endPoint,
+                linestring: linestring
+            };
+        },
+
+        _pointsOnOneLine: function (startPoint, endPoint, multiline) {
+            var isPointsOneLine = false;
+
+            array.forEach(multiline.components, lang.hitch(this, function (linestring) {
+                if (this._isPointContainsInLine(startPoint, linestring) &&
+                    this._isPointContainsInLine(endPoint, linestring)) {
+                    isPointsOneLine = linestring;
+                    return false;
+                }
+            }));
+
+            return isPointsOneLine;
         },
 
         _intersectsWithLayer: function (geometry, layer) {
@@ -118,16 +171,18 @@ define([
         },
 
         _isPointContainsInLinesLayer: function (point, linesLayer) {
+            var contained = false;
             array.forEach(linesLayer.features, lang.hitch(this, function (feature) {
                 if (this._isPointContainsInLine(point, feature.geometry)) {
+                    contained = true;
                     return true;
                 }
             }));
-            return false;
-        } ,
+            return contained;
+        },
 
         _isPointContainsInLine: function (point, line) {
             return point.distanceTo(line) === 0;
-        }
+        },
     });
 });
