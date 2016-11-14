65080960236429066ab5e8569878aefd620cf81d
Ivan Kavaliou <jownkiv@gmail.com>
2016-11-14 16:05:23 +0300

    #501 filling features was added to layer

--- nextgisweb_compulink/compulink_accepted_part/amd/ngw-compulink-accepted-part/layers/AcceptedPartsLayer.js
+++ nextgisweb_compulink/compulink_accepted_part/amd/ngw-compulink-accepted-part/layers/AcceptedPartsLayer.js
@@ -1,13 +1,14 @@
 define([
     'dojo/_base/declare',
     'dojo/_base/lang',
+    'dojo/_base/array',
     'dojo/on',
     'dojo/topic',
     'dojo/Evented',
     'dojo/Deferred',
     'ngw/openlayers',
     './_BaseLayer'
-], function (declare, lang, on, topic, Evented, Deferred, openlayers, _BaseLayer) {
+], function (declare, lang, array, on, topic, Evented, Deferred, openlayers, _BaseLayer) {
     return declare([_BaseLayer], {
         LAYER_NAME: 'AcceptedParts.AcceptedParts',
         DEFAULT_STYLE: {
@@ -15,8 +16,11 @@ define([
             strokeColor: '#ff0000',
             width: 2
         },
+        _featuresById: null,
 
         _bindEvents: function () {
+            this.inherited(arguments);
+
             topic.subscribe('compulink/accepted-parts/ui/layer/visibility/changed', lang.hitch(this, function (state) {
                 if (state) {
                     this._map.olMap.addLayer(this._layer);
@@ -24,6 +28,22 @@ define([
                 } else {
                     this._map.olMap.removeLayer(this._layer);
                 }
+            }));
+
+            this._store.on('fetched', lang.hitch(this, function (ngwFeatures) {
+                var feature,
+                    attributes;
+
+                this._featuresById = {};
+
+                array.forEach(ngwFeatures, function (ngwFeature) {
+                    feature = this.WKT.read(ngwFeature.geom);
+                    attributes = ngwFeature.fields;
+                    attributes.id = ngwFeature.id;
+                    feature.attributes = attributes;
+                    this._layer.addFeatures(feature);
+                    this._featuresById[attributes.id] = feature;
+                }, this);
             }));
         }
     });
