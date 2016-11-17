2fa9f72029a8e2f55cc76f0da5d278250eef7160
Ivan Kavaliou <jownkiv@gmail.com>
2016-11-17 21:53:58 +0300

    #508 uploading files functionality was implemented

--- nextgisweb_compulink/compulink_accepted_part/amd/ngw-compulink-accepted-part/ui/CreateAcceptedPartDialog/CreateAcceptedPartDialog.js
+++ nextgisweb_compulink/compulink_accepted_part/amd/ngw-compulink-accepted-part/ui/CreateAcceptedPartDialog/CreateAcceptedPartDialog.js
@@ -8,12 +8,19 @@ define([
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'ngw-compulink-site/ConfirmDialog',
+    'ngw-compulink-editor/editor/NgwServiceFacade',
+    'ngw-compulink-site/InfoDialog',
     'dojo/on',
     'dojo/text!./CreateAcceptedPartDialog.html',
+    'ngw-compulink-libs/dropzone/dropzone.min',
+    'xstyle/css!./CreateAcceptedPartDialog.css',
+    // 'xstyle/css!ngw-compulink-libs/dropzone/basic.css',
+    // 'xstyle/css!ngw-compulink-libs/dropzone/dropzone.min.css',
     'dojox/layout/TableContainer',
-    'dijit/form/TextBox'
-], function (declare, query, array, lang, html, _Widget, _TemplatedMixin, _WidgetsInTemplateMixin, ConfirmDialog, on,
-             template) {
+    'dijit/form/TextBox',
+    'dijit/form/Form'
+], function (declare, query, array, lang, html, _Widget, _TemplatedMixin, _WidgetsInTemplateMixin,
+             ConfirmDialog, NgwServiceFacade, InfoDialog, on, template, dropzone) {
     return declare([ConfirmDialog], {
         title: 'Создать принятый участок',
         message: 'Are you sure?',
@@ -31,6 +38,7 @@ define([
         acceptedPartAttributes: null,
         acceptedPartId: null,
         _mode: null,
+        _ngwServiceFacade: new NgwServiceFacade(),
 
         constructor: function (kwArgs) {
             lang.mixin(this, kwArgs);
@@ -67,8 +75,62 @@ define([
             this.inherited(arguments);
 
             if (this._mode === 'edit') {
-                this._fillAcceptedPartInputs(this.acceptedPartAttributes)
+                this._fillAcceptedPartInputs(this.acceptedPartAttributes);
+                this._initDropzone();
+            } else {
+                $('#ap_files').parents('tr').css('display', 'none');
             }
+        },
+
+        _dropzone: null,
+        _initDropzone: function () {
+            this._dropzone = new Dropzone('#ap_files', {
+                url: ngwConfig.applicationUrl + '/api/component/file_upload/upload',
+                dictDefaultMessage: 'Кликните для загрузки',
+                uploadMultiple: false
+            });
+
+            this._dropzone.on('success', lang.hitch(this, this._uploadSuccessHandler));
+            this._dropzone.on('error', lang.hitch(this, this._uploadErrorHandler));
+        },
+
+        _uploadSuccessHandler: function (fileResponse) {
+            var upload_meta,
+                attachmentInfo,
+                applyAttachmentToFeature;
+
+            upload_meta = JSON.parse(fileResponse.xhr.responseText).upload_meta[0];
+            attachmentInfo = {
+                file_upload: {
+                    id: upload_meta.id,
+                    size: upload_meta.size
+                },
+                mime_type: upload_meta.mime_type,
+                name: upload_meta.name,
+                size: upload_meta.size
+            };
+
+            applyAttachmentToFeature = this._ngwServiceFacade.applyAttachmentToFeature(
+                this.acceptedPartsStore._layerId,
+                this.acceptedPartId,
+                attachmentInfo
+            );
+
+            applyAttachmentToFeature.then(lang.hitch(this, function (result) {}),
+                lang.hitch(this, this._uploadErrorHandler));
+        },
+
+        _uploadErrorHandler: function () {
+            var files = this._dropzone.files,
+                countsFiles = files.length;
+
+            this._dropzone.removeFile(files[countsFiles - 1]);
+
+            new InfoDialog({
+                isDestroyedAfterHiding: true,
+                title: 'Сохранение файла',
+                message: 'Сохранить файл не удалось. Попробуйте еще раз.'
+            }).show();
         },
 
         _fillAcceptedPartInputs: function (acceptedPartAttributes) {
