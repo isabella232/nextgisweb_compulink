014adbefc9f5afa1f7e6c6042e29acfdaa5c1c52
Ivan Kavaliou <jownkiv@gmail.com>
2017-01-11 16:48:30 +0300

    #530 initAudioManager was overridden for turning off audio

--- nextgisweb_compulink/compulink_editor/amd/ngw-compulink-editor/player/recording_video/Timeline.js
+++ nextgisweb_compulink/compulink_editor/amd/ngw-compulink-editor/player/recording_video/Timeline.js
@@ -2,16 +2,23 @@ define([
     'dojo/_base/declare',
     'dojo/_base/lang',
     'dojo/topic',
+    'dojo/Deferred',
     '../Timeline',
     'xstyle/css!./Timeline.css'
-], function (declare, lang, topic, Timeline) {
+], function (declare, lang, topic, Deferred, Timeline) {
     return declare([Timeline], {
+        // audio should be off for recording video
+        initAudioManager: function () {
+            var deferred = new Deferred();
+            deferred.resolve();
+            return deferred;
+        },
+
         _bindEvents: function () {
             this.inherited(arguments);
 
             topic.subscribe('compulink/player/loaded', lang.hitch(this, function () {
                 setTimeout(lang.hitch(this, function () {
-                    this._audio.deactivate();
                     this._setParameters();
                     this._overrideGlobalFunctions();
                     this._setStoppedHandler();
