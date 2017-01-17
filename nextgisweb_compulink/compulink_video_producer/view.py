a4e34959743f7db0d21c88cb9410a852c7022d15
Ivan Kavaliou <jownkiv@gmail.com>
2017-01-17 22:51:42 +0300

    #532 added endpoints for video management with mock data

--- nextgisweb_compulink/compulink_video_producer/view.py
+++ nextgisweb_compulink/compulink_video_producer/view.py
@@ -1,19 +1,61 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
 from __future__ import unicode_literals
 
-def setup_pyramid(comp, config):
-    # config.add_route(
-    #     'compulink.get_layer_info',
-    #     '/compulink/accepted-parts/layer-info/{layer_type}/{construct_object_id:\d+}',
-    #     client=('layer_type', 'construct_object_id',)).add_view(get_layer_info)
-    #
-    # config.add_route(
-    #     'compulink.accepted_part_crud',
-    #     '/compulink/accepted-parts/{construct_object_id:\d+}/accepted-part/{accepted_part_id:\d+}',
-    #     client=('layer_type', 'construct_object_id', 'accepted_part_id',)) \
-    #     .add_view(create_accepted_part, request_method='PUT') \
-    #     .add_view(delete_accepted_part, request_method='DELETE') \
-    #     .add_view(update_accepted_part, request_method='POST')
-    pass
+import json
 
+from pyramid.response import Response
+
+from nextgisweb_compulink.utils import success_response
+
+
+def setup_pyramid(comp, config):
+    config.add_route(
+        'compulink.player.video.crud',
+        '/compulink/player/video',
+        client=('layer_type', 'construct_object_id', 'accepted_part_id',)) \
+        .add_view(get_video_list, request_method='GET') \
+        .add_view(make_video, request_method='DELETE') \
+        .add_view(remove_video, request_method='POST')
+
+
+def get_video_list(request):
+    # todo: get user info by session info and implement other logic
+    return Response(json.dumps([
+        {
+            'id': 1,
+            'name': u'Видео 15/01/2017',
+            'size': u'1 Мб',
+            'url': '#',
+            'created_date_time': 1484482931,
+            'status': 'ready'
+        },
+        {
+            'id': 2,
+            'name': u'Видео 18/01/2017',
+            'size': None,
+            'url': None,
+            'created_date_time': None,
+            'status': 'creating'
+        },
+        {
+            'id': 3,
+            'name': u'Видео 17/01/2017',
+            'size': u'3 Мб',
+            'url': '#',
+            'created_date_time': 1484680941,
+            'status': 'ready'
+        }
+    ]))
+
+
+def make_video(request):
+    # todo: get user info by session info and implement other logic
+    return success_response()
+
+
+def remove_video(request):
+    # todo: get user info by session info and implement other logic
+    return success_response()
