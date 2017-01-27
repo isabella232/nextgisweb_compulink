3e8891f11b6263df8e1b18940618f126167cfbf6
Ivan Kavaliou <jownkiv@gmail.com>
2017-01-27 23:10:59 +0300

    #516 sketch layer activation was added

--- nextgisweb_compulink/compulink_editor/amd/ngw-compulink-editor/player/PhotoTimeline/PhotoPointer/PhotoPointerControl.js
+++ nextgisweb_compulink/compulink_editor/amd/ngw-compulink-editor/player/PhotoTimeline/PhotoPointer/PhotoPointerControl.js
@@ -7,11 +7,41 @@ define([
     return openlayers.Class(openlayers.Control, {
         CLASS_NAME: 'OpenLayers.Control.PhotoPointer',
         template: mustache.parse(template),
+        _pointerLayer: null,
 
         draw: function () {
             var div = openlayers.Control.prototype.draw.apply(this);
             div.innerHTML = mustache.render(template, this);
             return div;
+        },
+
+        activate: function () {
+            openlayers.Control.prototype.activate.apply(this);
+            this._activateLayer();
+        },
+
+        deactivate: function () {
+            openlayers.Control.prototype.deactivate.apply(this);
+            this._deactivateLayer();
+        },
+
+        _activateLayer: function () {
+            this.map.addLayer(this._getLayer());
+        },
+
+        _deactivateLayer: function () {
+            this.map.removeLayer(this._getLayer());
+        },
+
+        _getLayer: function () {
+            if (!this._pointerLayer) {
+                this._pointerLayer = this._makeLayer();
+            }
+            return this._pointerLayer;
+        },
+
+        _makeLayer: function () {
+            return new openlayers.Layer.Vector(this.id);
         }
     });
 });
