b3bf5195a60369254b4aa4f392eccbcdb1b9b4f1
Ivan Kavaliou <jownkiv@gmail.com>
2017-02-28 13:14:00 +0300

    #549 AcceptedPartsTooltip was implemented

---
+++ nextgisweb_compulink/compulink_accepted_part/amd/ngw-compulink-accepted-part/ui/AcceptedPartsTooltip/AcceptedPartsTooltip.js
@@ -0,0 +1,38 @@
+define([
+    'dojo/_base/declare',
+    'xstyle/css!./AcceptedPartsTooltip.css'
+], function (declare) {
+    return declare(null, {
+        _map: null,
+        _$olMap: null,
+        _targetDiv: null,
+
+        constructor: function (map) {
+            this._map = map;
+        },
+
+        activate: function (message) {
+            var $html = $('<span class="accepted-parts-tooltip">' + message + '</span>');
+            this._$tooltip = $html.appendTo($(this._map.olMap.div).parent());
+            this._$olMap = $(this._map.olMap.div);
+
+            this._map.olMap.events.register('mousemove', this, this._mouseMoveHandler);
+        },
+
+        _mouseMoveHandler: function (e) {
+            var offsetOlMap = this._$olMap.offset();
+            this._$tooltip.css('left', e.pageX - offsetOlMap.left);
+            this._$tooltip.css('top', e.pageY - offsetOlMap.top);
+        },
+
+        updateMessage: function (message) {
+            this._$tooltip.html(message);
+        },
+
+        deactivate: function () {
+            this._map.olMap.events.unregister('mousemove', this, this._mouseMoveHandler);
+            this._map.olMap.events.unregister('movestart', this, this._moveStartHandler);
+            this._map.olMap.events.unregister('moveend', this, this._moveEndHandler);
+        }
+    });
+});
