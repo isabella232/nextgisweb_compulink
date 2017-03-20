99bb5bd45f9e63e912e8ef1e07c2de079d424c22
yellow-sky <nikulin.e@gmail.com>
2017-03-20 23:13:36 +0300

    Resolve #621

--- nextgisweb_compulink/compulink_video_producer/command.py
+++ nextgisweb_compulink/compulink_video_producer/command.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+
 from nextgisweb_compulink.compulink_video_producer.default_video_page import DefaultVideoPage, UNITS
+from nextgisweb_compulink.compulink_video_producer.model import VideoProduceTask
 from nextgisweb_compulink.compulink_video_producer.video_options import VideoOptions
 from nextgisweb_compulink.compulink_video_producer.video_producer import VideoProducer
+from nextgisweb_compulink.compulink_video_producer.view import remove_video_entry
 from nextgisweb.command import Command
+
+
+@Command.registry.register
+class RemoveOutdatedVideoCommand():
+    identity = 'compulink.remove_outdated_video'
+
+    @classmethod
+    def argparser_setup(cls, parser, env):
+        pass
+
+    @classmethod
+    def execute(cls, args, env):
+        days = int(env.compulink_video_producer.settings.get('video_outdated_period', 7))
+        tasks = VideoProduceTask.filter(VideoProduceTask.creation_dt < datetime.now() - timedelta(days=days))
+        c = 0
+        for task in tasks:
+            remove_video_entry(task.id)
+            c += 1
+        print('Video removed: ' + str(c))
 
 
 @Command.registry.register
