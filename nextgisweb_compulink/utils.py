7b3d7672810ce82b455fc4df7cc2c4ce5ce7db6f
Ivan Kavaliou <jownkiv@gmail.com>
2017-04-18 16:00:53 +0300

    pep-8 cleaning

--- nextgisweb_compulink/utils.py
+++ nextgisweb_compulink/utils.py
@@ -6,6 +6,7 @@ def error_response(mes):
     resp = {'status': 'error', 'message': mes}
     return Response(json.dumps(resp), status=400)
 
+
 def success_response():
     resp = {'status': 'ok'}
     return Response(json.dumps(resp))
